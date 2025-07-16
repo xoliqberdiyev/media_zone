4cae4d2729368abc1eb4ae62c7e4e7135527e348
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-07-16 11:52:37 +0500

    add domain to cors_allowed_origins

--- core/packages/cors_headers.py
+++ core/packages/cors_headers.py
@@ -5,6 +5,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://media-zone.vercel.app",
     'https://mediazonepro-tebk.vercel.app',
+    'https://media-zone-web.vercel.app'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
