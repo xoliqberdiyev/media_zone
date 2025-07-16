df974aa0d9d589bfe9bace1d4f5e3bc42df4f9a6
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-07-16 18:44:13 +0500

    add two domain

--- core/packages/cors_headers.py
+++ core/packages/cors_headers.py
@@ -5,7 +5,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://media-zone.vercel.app",
     'https://mediazonepro-tebk.vercel.app',
-    'https://media-zone-web.vercel.app'
+    'https://media-zone-web.vercel.app',
+    'https://crm.mediazonestudio.uz',
+    'https://mediazonestudio.uz',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
