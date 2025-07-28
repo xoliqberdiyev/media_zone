4d3c5ffd5140508a8aa6b94a885e555712e8cc9c
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-07-28 14:28:25 +0500

    add models delete method

--- apps/web/models.py
+++ apps/web/models.py
@@ -7,14 +7,31 @@ class Partner(BaseModel):
     icon = models.FileField(upload_to='media_zone/partners')
     instagram_link = models.URLField(null=True, blank=True)
 
+    def delete(self):
+        self.icon.delete()
+        return super().delete()
+
+
 class Image(BaseModel):
     image = models.ImageField(upload_to='media_zone/images')
 
+    def delete(self):
+        self.image.delete()
+        return super().delete()
+
+
 class Video(BaseModel):
     video = models.FileField(upload_to="media_zone/videos")
+
+    def delete(self):
+        self.video.delete()
+        return super().delete()
 
 
 class Team(BaseModel):
     name = models.CharField(max_length=250)
     image = models.ImageField(upload_to="media_zone/teams")
 
+    def delete(self):
+        self.image.delete()
+        return super().delete()
