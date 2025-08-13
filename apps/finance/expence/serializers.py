8f14f90d9cfa035f26112cb46f180a6d52346a9f
Abdumavlonov Abdurahmon <abdumavlonovabdurahmon75@gmail.com>
2025-08-13 15:06:19 +0500

    New error scound

--- apps/finance/expence/serializers.py
+++ apps/finance/expence/serializers.py
@@ -1,11 +1,31 @@
 from django.db import transaction
 from rest_framework import serializers
 from apps.finance.models import Expence, ExpenceCategory
+from datetime import datetime
+from django.db.models import Sum
 
 class ExpenceCategorySerializer(serializers.ModelSerializer):
+    total_price = serializers.SerializerMethodField()
+
     class Meta:
         model = ExpenceCategory
         fields = ['id', 'name', 'total_price']
+
+    def get_total_price(self, obj):
+        request = self.context.get('request')
+        start_date = request.query_params.get('start_date')
+        end_date = request.query_params.get('end_date')
+
+        queryset = Expence.objects.filter(category=obj)
+        if start_date and end_date:
+            try:
+                start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
+                end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
+                queryset = queryset.filter(date__range=[start_date, end_date])
+            except ValueError:
+                pass
+
+        return queryset.aggregate(total=Sum('price'))['total'] or 0
 
 class ExpenceCreateSerializer(serializers.Serializer):
     category_id = serializers.UUIDField()
