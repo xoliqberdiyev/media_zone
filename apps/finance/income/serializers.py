10ee1d4fef63c8fb603e9c3bcdb64483420cb7d5
Abdumavlonov Abdurahmon <abdumavlonovabdurahmon75@gmail.com>
2025-08-13 15:37:49 +0500

    Fix income finance

--- apps/finance/income/serializers.py
+++ apps/finance/income/serializers.py
@@ -57,18 +57,17 @@ class IncomeListSerializer(serializers.ModelSerializer):
 
     def get_total_price(self, obj):
         request = self.context.get('request')
-        category_id = obj.category.id
-        start_date = request.query_params.get('start_date')
-        end_date = request.query_params.get('end_date')
-
-        queryset = Income.objects.filter(category__id=category_id)
-        if start_date and end_date:
-            try:
-                start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
-                end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
-                queryset = queryset.filter(date__range=[start_date, end_date])
-            except ValueError:
-                pass
+        queryset = self.context['view'].get_queryset()  # Joriy filtrlangan queryset
+        if request and hasattr(request, 'query_params'):
+            start_date = request.query_params.get('start_date')
+            end_date = request.query_params.get('end_date')
+            if start_date and end_date:
+                try:
+                    start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
+                    end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
+                    queryset = queryset.filter(date__range=[start_date, end_date])
+                except ValueError:
+                    pass
 
         return queryset.aggregate(total=Sum('price'))['total'] or 0
 
