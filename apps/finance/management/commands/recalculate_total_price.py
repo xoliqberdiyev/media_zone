916e827e8062e68c73120046081ecfd560dd6294
Abdumavlonov Abdurahmon <abdumavlonovabdurahmon75@gmail.com>
2025-08-14 10:58:58 +0500

    New error

--- apps/finance/management/commands/recalculate_total_price.py
+++ apps/finance/management/commands/recalculate_total_price.py
@@ -1,21 +1,13 @@
 from django.core.management.base import BaseCommand
 from django.db.models import Sum
-from apps.finance.models import IncomeCategory, ExpenceCategory
+from apps.finance.models import IncomeCategory
 
 class Command(BaseCommand):
-    help = 'Recalculate total_price for all IncomeCategory and ExpenceCategory'
+    help = 'Recalculate total_price for all IncomeCategory'
 
     def handle(self, *args, **kwargs):
-        # IncomeCategory
         for category in IncomeCategory.objects.all():
             total = category.incomes.aggregate(total_price=Sum('price'))['total_price'] or 0
             category.total_price = total
             category.save()
             self.stdout.write(self.style.SUCCESS(f'Updated IncomeCategory {category.name}: {total}'))
-
-        # ExpenceCategory
-        for category in ExpenceCategory.objects.all():
-            total = category.expences.aggregate(total_price=Sum('price'))['total_price'] or 0
-            category.total_price = total
-            category.save()
-            self.stdout.write(self.style.SUCCESS(f'Updated ExpenceCategory {category.name}: {total}'))
